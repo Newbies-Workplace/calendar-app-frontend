fb20badf5334e01d2a63542e306eae0cb99edbd6
Kuba <jakub.a.niew@gmail.com>
2024-09-02 18:56:17 +0200

    first commit

---
+++ my-vue-app/src/App.vue
@@ -0,0 +1,13 @@
+<script setup>
+import MainWindow from './components/MainWindow.vue'
+  
+</script>
+
+<template>
+  <div>
+    
+  </div>
+  <MainWindow msg="Pierwszy test frontendu" />
+</template>
+
+
---
+++ my-vue-app/src/components/MainWindow.vue
@@ -0,0 +1,38 @@
+<script setup>
+import { ref } from 'vue'
+
+defineProps({
+  msg: String,
+  timestamp: ""
+})
+
+const count = ref(0)
+const count2 = ref(0)
+</script>
+
+<template>
+  <h1>{{ msg }}</h1>
+
+  <div class="card">
+    <!--<button type="button" @click="count++">count is {{ count }}</button> -->
+    <!--<button type="button" @click="count2++">count2 is {{ count2 }}</button> -->
+    <div class="form=group">
+        <label for="name">Nazwa eventu </label>
+        <input type="text" class="form-control" id="name" placeholder="wpisz nazwę"/>
+        <br>
+        <label for="guests">Liczba osób </label>
+        <input type="number" id="guests" name="guests" min="0" max="100" placeholder="0"/>
+        <br>
+        <label for="start">Start date: </label>
+        <input type="date" id="start" name="trip-start" value="datetime" min="datetime"/>
+        <br>
+        <label for="end">End date:</label>
+        <input type="date" id="end" name="trip-end" value="datetime" min="datetime" />
+    </div>
+    <br>
+    <button type="button" @click="">Submit (tbd)</button>
+  </div>
+
+  
+  
+</template>
