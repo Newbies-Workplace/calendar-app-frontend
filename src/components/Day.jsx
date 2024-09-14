abc5e7c27005cb439db4cdd378d5b5b6e2c95d98
Kuba <jakub.a.niew@gmail.com>
2024-09-14 15:30:52 +0200

    day

--- src/components/Day.jsx
+++ src/components/Day.jsx
@@ -1,23 +1,65 @@
-import '../App.css'
-import './day.css'
+import "../App.css";
+import style from "./day.module.css";
+import { useEffect, useState } from "react";
+{
+  /* 
+{
+    dayNumber: string;  
+    votes: [status,isCurrentUserVote,];
+} 
+*/
+}
+function Day(props) {
+  //console.log(JSON.stringify(props));
+  const [fill, setFill] = useState("bg-[#ebebec]");
+  const currentVote = props.votes.find((vote) => vote.isCurrentUserVote);
 
+  useEffect(() => {
+    if (props.votes.length <= 0) {
+      return;
+    }
+    if (
+      props.votes.every((vote) => {
+        return vote.status == true;
+      })
+    ) {
+      setFill("bg-green-500");
+    } else if (
+      props.votes.every((vote) => {
+        return vote.status == false;
+      })
+    ) {
+      setFill("bg-red-300");
+    } else {
+      setFill("bg-yellow-500");
+    }
+  }, [props.votes]);
 
-function Day(){
-    return (
-        <>
-        <div >
-        <day>
-        <div className="circle-row">
-            <div className='circle'></div>
-            <div className='circle'></div>
-            <div className='circle'></div>
+  return (
+    <>
+      <div
+        className={` block w-[100px] h-[100px] bg-no-repeat flex flex-col justify-between items-start relative p-1 rounded-lg border-4 ${fill} `}
+      >
+        <div className={style.circleRow}>
+          <div
+            className={`w-[16px] h-[16px] rounded-full border border-black ${currentVote === undefined ? "bg-[#ebebec]" : currentVote.status ? "bg-green-400" : "bg-red-500"}`}
+          />
+
+          {props.votes.map((value, index) => {
+            if (!value.isCurrentUserVote)
+              return (
+                <div
+                  key={index}
+                  className={`w-[16px] h-[16px] rounded-full  border-black ${value.status ? "bg-green-400" : "bg-red-500"}`}
+                />
+              );
+          })}
+          {/*${props.obj.color}*/}
         </div>
-        <div className="number">12</div>
-        </day>
-        </div>
-        
-        </>
-    )
+        <div className={style.number}>{props.dayNumber}</div>
+      </div>
+    </>
+  );
 }
 
-export default Day
+export default Day;
--- src/main.jsx
+++ src/main.jsx
@@ -1,15 +1,38 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import Frontpage from './Frontpage.jsx'
-import './index.css'
-import Day from './components/Day.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import Frontpage from "./Frontpage.jsx";
+import "./index.css";
+import Day from "./components/Day.jsx";
 
-createRoot(document.getElementById('root')).render(
+const votes = [{ status: true, isCurrentUserVote: true }];
+
+createRoot(document.getElementById("root")).render(
   <StrictMode>
     {/* <Frontpage />
     <button className='image close'> </button>
     <button className='image share'> </button>
     <button className='image calendar'> </button> */}
-    <Day></Day>
-  </StrictMode>,
-)
+    <Day
+      dayNumber="1"
+      votes={[
+        { status: true, isCurrentUserVote: true },
+        { status: true, isCurrentUserVote: false },
+      ]}
+    />
+    <Day
+      dayNumber="2"
+      votes={[
+        { status: false, isCurrentUserVote: true },
+        { status: false, isCurrentUserVote: false },
+      ]}
+    />
+    <Day
+      dayNumber="3"
+      votes={[
+        { status: true, isCurrentUserVote: true },
+        { status: false, isCurrentUserVote: false },
+      ]}
+    />
+    <Day dayNumber="3" votes={[]} />
+  </StrictMode>
+);
